03b295d689b1fc5180b33bce532092f0bb9c6e2a
iTzRusso10 <giusepperusso1003@gmail.com>
2023-06-24 01:50:26 +0200

    Work section in progress

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,12 +1,9 @@
 import React from "react";
+import resize from "../assets/resize.png";
 
 const Work = () => {
-
-
   return (
-    <div
-      className="w-full p-5 flex flex-col justify-center gap-5"
-    >
+    <div className="w-full p-5 flex flex-col justify-center gap-5">
       <div className="flex flex-col gap-3 justify-center items-center">
         <h1 className="text-[#B205BB] text-4xl font-black ">
           <span className="text-white text-4xl">My</span> Work
@@ -21,6 +18,12 @@ const Work = () => {
       <video controls className="rounded-lg">
         <source src="./Videos/video1.mp4" type="video/mp4" />
       </video>
+      <div className="flex gap-2 p-3 rounded-md  bg-[#B205BB] justify-center items-center mx-auto hover:bg-[#81028a] active:bg-[#67026e]">
+      <img src={resize} className="w-4 h-4 " />
+        <button className="text-white">
+          GitHub Repository
+        </button>
+      </div>
     </div>
   );
 };
