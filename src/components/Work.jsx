4edf211fdf68ee1f341eaf4b0817cbe0b6caa9fd
iTzRusso10 <giusepperusso1003@gmail.com>
2023-06-24 03:00:08 +0200

    Fix: Padding

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,7 +4,7 @@ import video from '../../public/video/minigames-react-video.mp4'
 
 const Work = () => {
   return (
-    <div className="w-full p-10 flex flex-col justify-center gap-5 z-[1]">
+    <div className="w-full p-8 flex flex-col justify-center gap-5 z-[1]">
       <div className="flex flex-col gap-3 justify-center items-center">
         <h1 className="text-[#B205BB] text-4xl font-black ">
           <span className="text-white text-4xl">My</span> Work
