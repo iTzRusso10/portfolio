9d605f10a5bf6916a6c8372e47691d28dc92206e
iTzRusso10 <giusepperusso1003@gmail.com>
2023-06-25 19:02:24 +0200

    Continue Fixinf

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,9 +6,9 @@ import RobotCanvas from "./canvas/Robot";
 
 const Hero = () => {
   return (
-    <section className="flex h-[100svh] w-full flex-col items-center justify-center md:justify-start md:h-full lg:flex-row">
+    <section className="flex h-[100svh] w-full flex-col items-center justify-center md:justify-start md:h-full lg:flex-row lg:h-[100svh]">
       <div className="mt-28 flex flex-col items-center gap-2 px-6 max-[327px]:mt-16  md:justify-start md:gap-10 md:p-6 lg:mt-0 lg:w-full lg:gap-4 lg:p-6">
-        <h1 className="max-[350px]:text-md text-4xl font-black text-white max-[350px]:text-center">
+        <h1 className="max-[350px]:text-md text-4xl font-black text-white max-[350px]:text-center p-2">
           GIUSEPPE <span className="text-[#B205BB]"> RUSSO</span>
         </h1>
         <p className="text-center text-[12px] text-white md:px-6 md:text-base lg:px-12 lg:text-base">
