50952c691eb94413b51d7d918ed9f99566ff8557
iTzRusso10 <giusepperusso1003@gmail.com>
2023-06-25 20:51:40 +0200

    Add Scroll Animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import RobotCanvas from "./canvas/Robot";
 
 const Hero = () => {
   return (
-    <section className="flex h-full w-full flex-col items-center justify-center md:h-full md:justify-start lg:justify-center lg:h-[100svh] lg:flex-row">
+    <section className="flex h-full w-full flex-col items-center justify-center md:h-full md:justify-start lg:h-[100svh] lg:flex-row lg:justify-center">
       <div className="mt-28 flex flex-col items-center gap-2 px-6 max-[327px]:mt-16  md:justify-start md:gap-10 md:p-6 lg:mt-0 lg:w-full lg:gap-4 lg:p-6">
         <h1 className="max-[350px]:text-md p-2 text-4xl font-black text-white max-[350px]:text-center">
           GIUSEPPE <span className="text-[#B205BB]"> RUSSO</span>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,36 +1,63 @@
 import React from "react";
 import resize from "../assets/resize.png";
-import video from '../video/minigames-react-video.mp4'
-import poster from '../img/poster-video.png'
+import video from "../video/minigames-react-video.mp4";
+import poster from "../img/poster-video.png";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
 
 const Work = () => {
+  const [ref, inView] = useInView({
+    triggerOnce: false, // Cambia a false se vuoi che l'animazione si attivi ogni volta che l'elemento entra nella vista
+    threshold: 0.2, // Cambia in base a quando vuoi che l'animazione si attivi (0.1 indica che l'animazione si attiverà quando almeno il 10% dell'elemento è visibile)
+  });
+
+  const variants = {
+    hidden: { opacity: 0, y: 300 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    exit: { opacity: 0, y: 100, transition: { duration: 0.5 } },
+  };
+
   return (
-    <div className="w-full py-12 px-6 flex flex-col justify-center gap-5 z-[1]">
-      <div className="flex flex-col gap-3 justify-center items-center">
-        <h1 className="text-[#B205BB] text-4xl font-black ">
-          <span className="text-white text-4xl">My</span> Work
-        </h1>
-        <p className="text-white text-center">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
-          possimus inventore nam cum error veniam, magni veritatis id. Expedita,
-          eaque repudiandae. Doloremque exercitationem architecto, sint
-          similique quos eligendi ullam rerum?
-        </p>
-      </div>
-      <video controls className="rounded-lg" preload="auto" poster={poster}>
-        <source src={video} type="video/mp4" />
-      </video>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://github.com/iTzRusso10/Minigames-React"
-        className="flex"
+    <div ref={ref}>
+      <motion.div
+        className="z-[1] flex w-full flex-col justify-center gap-5 px-6 py-12"
+        ref={ref}
+        variants={variants}
+        initial="hidden"
+        animate={inView ? "show" : "hidden"}
       >
-        <div className="flex gap-2 p-3 rounded-md cursor-pointer  bg-[#B205BB] justify-center items-center mx-auto hover:bg-[#81028a] active:bg-[#67026e]">
-          <img src={resize} className="w-4 h-4 " />
-          <button className="text-white">GitHub Repository</button>
+        <div className="flex flex-col items-center justify-center gap-3">
+          <h1 className="text-4xl font-black text-[#B205BB] ">
+            <span className="text-4xl text-white">My</span> Work
+          </h1>
+          <p className="text-center text-white">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
+            possimus inventore nam cum error veniam, magni veritatis id.
+            Expedita, eaque repudiandae. Doloremque exercitationem architecto,
+            sint similique quos eligendi ullam rerum?
+          </p>
         </div>
-      </a>
+        <video controls className="rounded-lg" preload="auto" poster={poster}>
+          <source src={video} type="video/mp4" />
+        </video>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/iTzRusso10/Minigames-React"
+          className="flex"
+        >
+          <div className="mx-auto flex cursor-pointer items-center justify-center  gap-2 rounded-md bg-[#B205BB] p-3 hover:bg-[#81028a] active:bg-[#67026e]">
+            <img src={resize} className="h-4 w-4 " />
+            <button className="text-white">GitHub Repository</button>
+          </div>
+        </a>
+      </motion.div>
     </div>
   );
 };
